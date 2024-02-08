d05177da55de1b437fd4a8337079584f7292f9c8
Ryder <faiz64868@gmail.com>
2024-02-08 12:18:22 +0530

    commit 1
    initial Phase

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h6 className="w-full h-screen flex justify-center items-center bg-black text-white">
+        Project Live
+      </h6>
+    </>
+  );
+}
+
+export default App;
