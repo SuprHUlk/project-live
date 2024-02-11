f29bab9a544d04a1ae419600bc29c589267fe0bf
Ryder <faiz64868@gmail.com>
2024-02-11 10:25:12 +0530

    Added : navbar Search and go live menu

---
+++ frontend/src/renders/GoLiveMenu/GoliveDashboard.tsx
@@ -0,0 +1,13 @@
+export default function GoliveDashboard() {
+  return (
+    <>
+      <div className="w-[100%] h-[92vh] bg-black/80 flex justify-center items-center absolute top-[8vh]">
+        <div className="w-[50%] h-[85vh] bg-[#18181B] rounded-md border-[1px] border-gray-800">
+          <div className="w-[100%] text-white h-[5vh] flex justify-start items-center  pl-[5%] text-xl border-b-[1px] border-gray-500">
+            Create Your Stream
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
