a6a85583f1f7eadf4133f6ead04b23444307e80b
Ryder <faiz64868@gmail.com>
2024-02-15 03:21:10 +0530

    Fixed

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -51,39 +51,38 @@ const Navbar: React.FC<Props> = ({ toggleForm }) => {
         <div className="text-white font-bold w-[20%] h-[8vh] flex justify-between items-center pl-7">
           PROJECT LIVE
         </div>
-        {isDashboard && (
-          <div className="w-[20%] h-[8vh] flex justify-between items-center">
-            <CiSearch className="text-white relative left-[10%] text-2xl" />
-            <input
-              className="w-[100%] h-[5vh] bg-[#0E0E10] rounded-md border-[1px] border-gray-900 pl-[12%] text-white hover:bg-[#414146]"
-              placeholder="Search"
-            ></input>
-          </div>
-        )}
+        {isDashboard ||
+          (isSetting && (
+            <div className="w-[20%] h-[8vh] flex justify-between items-center">
+              <CiSearch className="text-white relative left-[10%] text-2xl" />
+              <input
+                className="w-[100%] h-[5vh] bg-[#0E0E10] rounded-md border-[1px] border-gray-900 pl-[12%] text-white hover:bg-[#414146]"
+                placeholder="Search"
+              ></input>
+            </div>
+          ))}
         <div className="w-[20%] h-[8vh]  flex justify-center items-center  ">
           <div className="w-[50%] h-[8vh] flex justify-center items-center gap-3">
-            {!isDashboard ||
-              (!isSetting && (
-                <button
-                  className="w-[50%] h-8 bg-[#2F2F35] rounded-md text-white shadow-2xl border-[1px] border-black hover:bg-[#414146]"
-                  onClick={() => toggleForm!("login")}
-                >
-                  Log In
-                </button>
-              ))}
+            {!isDashboard && !isSetting && (
+              <button
+                className="w-[50%] h-8 bg-[#2F2F35] rounded-md text-white shadow-2xl border-[1px] border-black hover:bg-[#414146]"
+                onClick={() => toggleForm!("login")}
+              >
+                Log In
+              </button>
+            )}
 
-            {!isDashboard ||
-              (!isSetting && (
-                <button
-                  className="w-[50%] h-8 bg-blue-600 rounded-md text-white shadow-2xl border-[1px] border-black hover:bg-blue-800"
-                  onClick={() => toggleForm!("signup")}
-                >
-                  Sign Up
-                </button>
-              ))}
+            {!isDashboard && !isSetting && (
+              <button
+                className="w-[50%] h-8 bg-blue-600 rounded-md text-white shadow-2xl border-[1px] border-black hover:bg-blue-800"
+                onClick={() => toggleForm!("signup")}
+              >
+                Sign Up
+              </button>
+            )}
           </div>
-          {isDashboard ||
-            (isSetting && (
+          {!isDashboard ||
+            (!isSetting && (
               <Button
                 color="error"
                 variant="contained"
@@ -105,33 +104,32 @@ const Navbar: React.FC<Props> = ({ toggleForm }) => {
       </div>
 
       <div
-        className={`w-[10%] h-max bg-[#2F2F35] shadow-lg shadow-black  absolute top-[9vh] right-[1%] ${
+        className={`w-[10%] h-max bg-[#2F2F35] shadow-lg shadow-black absolute top-[9vh] right-[1%] ${
           isMenuVisible ? "" : "hidden"
         }`}
       >
-        {isDashboard ||
-          (isSetting && (
-            <button className="text-white flex justify-start items-center w-[100%] h-max border-b-[1px] border-gray-600 hover:bg-slate-700">
-              <CiUser className="m-2 text-xl" /> {username}
+        {(isDashboard || isSetting) && (
+          <button className="text-white flex justify-start items-center w-[100%] h-max border-b-[1px] border-gray-600 hover:bg-slate-700">
+            <CiUser className="m-2 text-xl" /> {username}
+          </button>
+        )}
+
+        {(isDashboard || isSetting) && (
+          <Link to="/settings">
+            <button className="text-white flex justify-start items-center w-[100%] h-[5vh] border-b-[1px] border-gray-600 hover:bg-slate-700">
+              <CiSettings className="m-2 text-xl" /> Settings
             </button>
-          ))}
-        {isDashboard ||
-          (isSetting && (
-            <Link to="/settings">
-              <button className="text-white flex justify-start items-center w-[100%] h-[5vh] border-b-[1px] border-gray-600 hover:bg-slate-700">
-                <CiSettings className="m-2 text-xl" /> Settings
-              </button>
-            </Link>
-          ))}
-        {isDashboard ||
-          (isSetting && (
-            <button
-              className="text-white flex justify-start items-center w-[100%] h-[5vh] hover:bg-slate-700"
-              onClick={handleLogout}
-            >
-              <CiLogout className="m-2 text-xl" /> Logout
-            </button>
-          ))}
+          </Link>
+        )}
+
+        {(isDashboard || isSetting) && (
+          <button
+            className="text-white flex justify-start items-center w-[100%] h-[5vh] hover:bg-slate-700"
+            onClick={handleLogout}
+          >
+            <CiLogout className="m-2 text-xl" /> Logout
+          </button>
+        )}
       </div>
       {isGoliveDashboardVisible && <GoliveDashboard />}
     </>
