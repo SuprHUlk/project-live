93caa5acd361a9c519b53ece87f871426333950c
Ryder <faiz64868@gmail.com>
2024-04-13 15:06:50 +0530

    commit

--- frontend/src/renders/showcad/Irlshow.tsx
+++ frontend/src/renders/showcad/Irlshow.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button, Chip } from "@mui/material";
 import imgjs from "../../assets/justchatting.webp";
 import pfp from "../../assets/pfp.jpg";
+import { Link } from "react-router-dom";
 
 // Sample array of tags
 const tagsArray = ["Gaming", "Music", "Art", "Fitness"];
@@ -48,38 +49,42 @@ export default function Irlshow() {
         <div className="w-[100%] h-max mt-2 flex gap-5 flex-wrap justify-start">
           {/* Stream templete here  */}
           <div className="w-[30%] h-[50vh] ml-5">
-            <div className="w-[100%] h-[30vh] bg-green-200"></div>
-            <div className="w-[100%]h-[20vh]">
-              <div className="w-[100%] h-[10vh] mt-2 flex justify-start items-center">
-                <img
-                  src={pfp}
-                  className="w-[10%] ml-2 rounded-[50%]"
-                  alt="Profile"
-                />
-                <div className="text-xl font-bold w-[100%] ml-3 text-gray-500 hover:text-blue-400 hover:cursor-pointer">
-                  Faiz Khan is Live
+            <Link to="#">
+              <div className="w-[100%] h-[30vh] bg-green-200"></div>
+              <div className="w-[100%]h-[20vh]">
+                <div className="w-[100%] h-[10vh] mt-2 flex justify-start items-center">
+                  <img
+                    src={pfp}
+                    className="w-[10%] ml-2 rounded-[50%]"
+                    alt="Profile"
+                  />
+                  <div className="text-xl font-bold w-[100%] ml-3 text-gray-500 hover:text-blue-400 hover:cursor-pointer">
+                    Faiz Khan is Live
+                  </div>
+                </div>
+                <div className="ml-[3%] font-bold">Just Chatting</div>
+                <div className="100% h-max mt-2">
+                  {/* Chip tags */}
+                  {tagsArray.map((tag, index) => (
+                    <Chip
+                      key={index}
+                      label={tag}
+                      onClick={() => handleTagClick(tag)}
+                      variant={
+                        selectedTags.includes(tag) ? "outlined" : "filled"
+                      }
+                      className="m-1"
+                      sx={{
+                        backgroundColor: "#24272C",
+                        color: "white",
+                        marginLeft: "1%",
+                      }}
+                      color={selectedTags.includes(tag) ? "primary" : "default"}
+                    />
+                  ))}
                 </div>
               </div>
-              <div className="ml-[3%] font-bold">Just Chatting</div>
-              <div className="100% h-max mt-2">
-                {/* Chip tags */}
-                {tagsArray.map((tag, index) => (
-                  <Chip
-                    key={index}
-                    label={tag}
-                    onClick={() => handleTagClick(tag)}
-                    variant={selectedTags.includes(tag) ? "outlined" : "filled"}
-                    className="m-1"
-                    sx={{
-                      backgroundColor: "#24272C",
-                      color: "white",
-                      marginLeft: "1%",
-                    }}
-                    color={selectedTags.includes(tag) ? "primary" : "default"}
-                  />
-                ))}
-              </div>
-            </div>
+            </Link>
           </div>
           {/* Here the templete end  */}
         </div>
